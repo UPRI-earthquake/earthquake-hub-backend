83d06bb28f522afa877f3d296aa04b8f852c8609
Jeff Sanchez (sysop@hp) <sanchez.christopherjeff@gmail.com>
2021-08-22 21:37:27 +0800

    refactor: Change default listening port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const stationsRouter = require('./routes/stations');
 const eventsRouter = require('./routes/events');
