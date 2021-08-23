11c5fffc2094c6e1f4d046b205df83a2e2777142
Jeff Sanchez (sysop@hp) <sanchez.christopherjeff@gmail.com>
2021-08-23 21:54:37 +0800

    feat: Allow CORS with localhost

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
 const port = process.env.PORT || 5000;
 
 const stationsRouter = require('./routes/stations');
 const eventsRouter = require('./routes/events');
 
+app.use(cors({origin: 'http://localhost:3000'}))
 app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.json({'message': 'ok'});
