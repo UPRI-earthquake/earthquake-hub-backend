7e76b2b763135a5fb75d5366162cce70f8864483
Jeff Sanchez (sysop@hp) <sanchez.christopherjeff@gmail.com>
2021-09-09 17:56:12 +0800

    style: Make query more readable

--- services/events.js
+++ services/events.js
@@ -5,23 +5,33 @@ const axios = require('axios');
 // get magnitude, coord, time, 
 async function getEventsList(startTime, endTime){
   const [rows, fields] = await db.query(
-    `select PEvent.publicID, Origin.time_value as OT, 
-        Origin.latitude_value,Origin.longitude_value, Origin.depth_value,
-        Magnitude.magnitude_value, Magnitude.type, EventDescription.text 
-            from Origin,PublicObject as POrigin, 
-                 Event, PublicObject as PEvent, 
-                 Magnitude, PublicObject as PMagnitude,
-                 EventDescription
-            where 
-                Event._oid=PEvent._oid 
-                and Origin._oid=POrigin._oid 
-                and Magnitude._oid=PMagnitude._oid 
-                and PMagnitude.publicID=Event.preferredMagnitudeID 
-                and POrigin.publicID=Event.preferredOriginID
-                and Event._oid=EventDescription._parent_oid
-                and EventDescription.type='region name'
-                and Origin.time_value >= ?
-                and Origin.time_value <= ?`        
+    `select 
+       PEvent.publicID, 
+       Origin.time_value as OT, 
+       Origin.latitude_value,
+       Origin.longitude_value,
+       Origin.depth_value,
+       Magnitude.magnitude_value, 
+       Magnitude.type, 
+       EventDescription.text 
+     from 
+       Origin,
+       PublicObject as POrigin, 
+       Event,
+       PublicObject as PEvent, 
+       Magnitude,
+       PublicObject as PMagnitude,
+       EventDescription
+     where 
+       Event._oid=PEvent._oid 
+       and Origin._oid=POrigin._oid 
+       and Magnitude._oid=PMagnitude._oid 
+       and PMagnitude.publicID=Event.preferredMagnitudeID 
+       and POrigin.publicID=Event.preferredOriginID
+       and Event._oid=EventDescription._parent_oid
+       and EventDescription.type='region name'
+       and Origin.time_value >= ?
+       and Origin.time_value <= ?`        
     , [startTime, endTime]); // automatic escaping when using placeholders
   const data = helper.emptyOrRows(rows);
 
@@ -36,6 +46,7 @@ async function addPlaces(eventsList){
     const result = await axios.get(
       'http://localhost:8080/reverse?format=jsonv2' 
        +`&lat=${event.latitude_value}&lon=${event.longitude_value}`
+       +'&zoom=10'
     );
     let address = '';
     if (result.data.error){ address = result.data.error }
