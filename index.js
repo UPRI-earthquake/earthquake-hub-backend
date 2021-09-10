b8ddeb8f45c066ae482f45be91831c448cf4d0c7
Jeff Sanchez (sysop@hp) <sanchez.christopherjeff@gmail.com>
2021-09-10 19:21:22 +0800

    feat: Incorporate EVENTS messages to proxy

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const port = process.env.PORT || 5000;
 
 const stationsRouter = require('./routes/stations');
 const eventsRouter = require('./routes/events');
+const messagingRouter = require('./routes/messaging');
 
-app.use(cors({origin: 'http://localhost:3000'}))
+app.use(cors({origin:'*'}))//{origin: 'http://localhost:3000'}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -20,44 +21,7 @@ app.get('/', (req, res) => {
 // ROUTES
 app.use('/stationLocations', stationsRouter)
 app.use('/eventsList', eventsRouter)
-
-// Setup Redis pubsub subscriber, and Event Emitter
-const redis_channel = "PICK"
-const redis_host = "172.17.0.2"
-const subscriber = redis.createClient({host:redis_host})
-subscriber.subscribe(redis_channel)
-
-// create helper middleware so we can reuse server-sent events
-const useServerSentEventsMiddleware = (req, res, next) => {
-    res.setHeader('Content-Type', 'text/event-stream');
-    res.setHeader('Cache-Control', 'no-cache');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.flushHeaders();
-
-    const sendEventStreamData = (eventName, data) => {
-      res.write(`event: ${eventName}\n`);
-      res.write(`data: ${data}\n\n`);
-    }
-
-    // we are attaching sendEventStreamData to res, so we can use it later
-    Object.assign(res, {
-      sendEventStreamData
-    });
-
-    next();
-}
-
-app.get('/messaging', useServerSentEventsMiddleware, (req, res) => {
-  subscriber.on("message", (channel, message) => {
-    console.log(JSON.parse(message));
-    res.sendEventStreamData(message)
-  });
-
-  // client close event
-  res.on('close', () => {
-    res.end();
-  });
-});
+app.use('/messaging', messagingRouter)
 
 /* Error handler middleware */
 app.use((err, req, res, next) => {
---
+++ routes/messaging.js
@@ -0,0 +1,46 @@
+const redis = require('redis');
+const express = require('express');
+const router = express.Router();
+
+// Setup Redis pubsub subscriber, and Event Emitter
+const redis_channel = "SC_*" // SC_PICK or SC_EVENT
+const redis_host = "172.17.0.2"
+const subscriber = redis.createClient({host:redis_host})
+subscriber.psubscribe(redis_channel)
+
+// create helper middleware so we can reuse server-sent events
+const useServerSentEventsMiddleware = (req, res, next) => {
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.flushHeaders();
+
+    const sendEventStreamData = (eventName, data) => {
+      res.write(`event: ${eventName}\n`);
+      res.write(`data: ${data}\n\n`);
+    }
+
+    // we are attaching sendEventStreamData to res, so we can use it later
+    Object.assign(res, {
+      sendEventStreamData
+    });
+
+    next();
+}
+
+router.get('/', useServerSentEventsMiddleware, (req, res) => {
+  console.log('SSE connection opened:', req.ip)
+  function logAndSend(pattern, channel, message) {
+    console.log(`Sending SSE: ${req.ip}\n`, JSON.parse(message));
+    res.sendEventStreamData(channel,message)
+  }
+  subscriber.on("pmessage", logAndSend);
+
+  res.on('close', () => {
+    subscriber.removeListener('pmessage', logAndSend)
+    res.end();
+    console.log('SSE connection closed:', req.ip)
+  });
+});
+
+module.exports = router
