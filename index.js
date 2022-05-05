dc465a19970463df7454b4fbb5e07a67bda609ec
jeffsanchez <sanchez.christopherjeff@gmail.com>
2022-05-05 15:36:43 +0800

    feat: Use environment vars for hosts and ports

--- index.js
+++ index.js
@@ -6,16 +6,18 @@ const cors = require('cors');
 require('dotenv').config({path: __dirname + '/.env'})
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.NODE_ENV === 'production'
+             ? process.env.BACKEND_PROD_PORT
+             : process.env.BACKEND_DEV_PORT;
 
 const stationsRouter = require('./routes/stations');
 const eventsRouter = require('./routes/events');
 const messagingRouter = require('./routes/messaging');
 const notifs  = require('./routes/notifications');
 
-app.use(cors({origin : process.env.NODE_ENV === 'production' 
+app.use(cors({origin : process.env.NODE_ENV === 'production'
   ? 'https://' + process.env.CLIENT_PROD_HOST
-  : [ /*'https://' + process.env.CLIENT_PROD_HOST,*/ 'https://localhost']
+  : 'https://' + process.env.CLIENT_DEV_HOST +":"+ process.env.CLIENT_DEV_PORT
 }))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -46,6 +48,9 @@ http.createServer(app).listen(port, () => {
   if (process.env.NODE_ENV === 'production'){
     console.log(
       'Production backend listening at '
+    + `http://backend:${port}`
+    console.log(
+      'Accessible through nginx at '
     + `https://${process.env.BACKEND_PROD_HOST}`
     )
   }else{
