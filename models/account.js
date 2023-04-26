e4a765ba6a8b698dfcdc1cbf89c2d5cc888eeb25
Paolo Daniel D. Bito <paolodanield.bito@Paolos-MacBook-Pro.local>
2023-04-26 15:28:56 +0800

    feat: Added models used in previous PRs

---
+++ models/account.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const { Schema, model } = mongoose;
+const { Types } = Schema;
+
+const AccountSchema = new Schema({
+  username: String,
+  email: String,
+  password: String,
+  accountStatus: {
+    type: String,
+    default: 'Inactive'
+  },
+  devicesCount: {
+    type: Number,
+    default: 0
+  },
+  devices: [{
+    type: Types.ObjectId,
+    ref: 'Device'
+  },],
+})
+
+module.exports = model('Account', AccountSchema)
