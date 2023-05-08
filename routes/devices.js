e2a2d32b7c065edb66cf91c09123d19b5634baa5
Jeff Sanchez (sysop@hp) <sanchez.christopherjeff@gmail.com>
2023-05-08 22:50:47 +0800

    feat: add token verification middlewares

--- routes/devices.js
+++ routes/devices.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const { addDevice, linkDevice } = require('../services/devices');
+const {
+  getTokenFromCookie,
+  getTokenFromBearer,
+  verifyTokenWithRole
+} = require('../middlewares/token.middleware')
 
 const deviceRouter = express.Router();
 
-deviceRouter.post('/add', addDevice);
-deviceRouter.post('/link', linkDevice);
+// Citizen users should have verified token to add devices to their profile via webapp
+deviceRouter.route('/add').post(
+  getTokenFromCookie,
+  verifyTokenWithRole('citizen'),
+  addDevice
+);
+
+// Sensor devices that will request for linking with a citizen acct requires bearer token
+deviceRouter.route('/link').post(
+  getTokenFromBearer,
+  verifyTokenWithRole('sensor'),
+  linkDevice
+);
 
 module.exports = deviceRouter;
