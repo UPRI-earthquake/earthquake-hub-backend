24481a62402515f7c1f1a7ca7a3e4d080508feb8
Jeff Sanchez (sysop@hp) <sanchez.christopherjeff@gmail.com>
2023-05-09 15:43:17 +0800

    refactor: add verbose message to confirmPassword

--- routes/accounts.route.js
+++ routes/accounts.route.js
@@ -17,7 +17,8 @@ const {
 const registerSchema = Joi.object({
   username: Joi.string().required(),
   password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{6,30}$')).required(),
-  confirmPassword: Joi.equal(Joi.ref('password')).required(),
+  confirmPassword: Joi.equal(Joi.ref('password')).required()
+                   .messages({"any.only": "Passwords should match."}),
   email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required()
 });
 
