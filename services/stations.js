93a341a6d0f9a1d6d87dd324919fd25d038e7232
chrscbs <johnchristiancabang@gmail.com>
2023-06-23 14:03:24 +0800

    chore: delete previous getStationLocations() function, remove stations.model

--- services/stations.js
+++ services/stations.js
@@ -1,16 +1,3 @@
-// const db = require('./mysqldb');
-// const helper = require('../helper');
-
-// async function getStationLocations(){
-//   const [rows, fields] = await db.query(
-//        'SELECT code, latitude, longitude, description'
-//     + ' FROM Station AS table1'
-//     + ' WHERE end IS NULL' // metadata with no end is presently used
-//   );
-//   const data = helper.emptyOrRows(rows);
-//   return data
-// }
-
 const device = require('../models/device.model');
 const account = require('../models/account.model');
 
