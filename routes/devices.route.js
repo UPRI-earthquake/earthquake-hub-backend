546cb9ff72a82b35346ae18a4ba11900bceb3be5
chrscbs <johnchristiancabang@gmail.com>
2023-07-18 23:45:25 +0800

    feat: add devices schema documentation and GET /device/status endpoint documentation

--- routes/devices.route.js
+++ routes/devices.route.js
@@ -1,3 +1,101 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Device:
+ *       type: object
+ *       properties:
+ *         _id:
+ *           type: string
+ *           description: The auto-generated ObjectId of the device
+ *         description:
+ *           type: string
+ *           description: The description of the device
+ *         streamId:
+ *           type: string
+ *           description: The stream ID of the device (e.g. AM_RE722\.*\/MSEED)
+ *         network:
+ *           type: string
+ *           description: The network of the device
+ *         station:
+ *           type: string
+ *           description: The station of the device
+ *         latitude:
+ *           type: number
+ *           description: The latitude of the device
+ *         longitude:
+ *           type: number
+ *           description: The longitude of the device
+ *         elevation:
+ *           type: number
+ *           description: The elevation of the device
+ *         macAddress:
+ *           type: string
+ *           description: The MAC address of the device
+ *         activity:
+ *           type: string
+ *           description: The activity status of the device (active, inactive, INTERNAL_ERROR)
+ *         activityToggleTime:
+ *           type: string
+ *           format: date-time
+ *           description: The time of the last activity toggle for the device
+ */
+
+/**
+ * @swagger
+ * /device/status:
+ *   get:
+ *     summary: Get the status of a specified device
+ *     tags:
+ *       - Devices
+ *     parameters:
+ *       - in: query
+ *         name: network
+ *         schema:
+ *           type: string
+ *         description: The network code of the device
+ *         required: true
+ *         example: "AM"
+ *       - in: query
+ *         name: station
+ *         schema:
+ *           type: string
+ *         description: The station code of the device
+ *         required: true
+ *         example: "RE722"
+ *     responses:
+ *       200:
+ *         description: Successful response with device network, station, status, and statusSince sent back as payload
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: number
+ *                   description: The HTTP status code
+ *                 message:
+ *                   type: string
+ *                   description: A descriptive message
+ *                 payload:
+ *                   type: object
+ *                   description: Data payload
+ *       500:
+ *         description: Station not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: number
+ *                   description: The HTTP status code
+ *                 message:
+ *                   type: string
+ *                   description: A descriptive message
+ */
+
+
 const express = require('express');
 const { addDevice, linkDevice, getDeviceList, getDeviceStatus } = require('../services/devices.service');
 const {
