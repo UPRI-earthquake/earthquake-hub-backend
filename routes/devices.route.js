6969d55c23b05d6d6b2827f0ddba78b43441417f
chrscbs <johnchristiancabang@gmail.com>
2023-07-19 18:35:42 +0800

    chore: add swagger ui documentation to /device/add and /device/list endpoints

--- routes/devices.route.js
+++ routes/devices.route.js
@@ -41,6 +41,130 @@
  *           description: The time of the last activity toggle for the device
  */
 
+const express = require('express');
+const { addDevice, linkDevice, getDeviceList, getDeviceStatus } = require('../services/devices.service');
+const {
+  getTokenFromCookie,
+  getTokenFromBearer,
+  verifyTokenWithRole
+} = require('../middlewares/token.middleware')
+
+const deviceRouter = express.Router(); 
+
+/**
+ * @swagger
+ * /device/add:
+ *   post:
+ *     summary: Endpoint for adding a device to the user's profile
+ *     tags:
+ *       - Devices
+ *     security:
+ *       - cookieAuth: []
+ *     requestBody:
+ *       description: Device data to be added
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               network:
+ *                 type: string
+ *                 description: The network code of the device
+ *               station:
+ *                 type: string
+ *                 description: The station code of the device
+ *               latitude:
+ *                 type: string
+ *                 description: The latitude of the device (in degree coordinates)
+ *               longitude:
+ *                 type: string
+ *                 description: The longitude of the device (in degree coordinates)
+ *               elevation:
+ *                 type: string
+ *                 description: The elevation of the device (in meters)
+ *           example:
+ *             network: "AM"
+ *             station: "R3B2D"
+ *             latitude: "40.123456"
+ *             longitude: "120.654321"
+ *             elevation: "50"
+ *     responses:
+ *       200:
+ *         description: Device added successfully
+ *       400:
+ *         description: Device is already added to the database
+ *       403:
+ *         description: Unauthorized - Invalid or missing token
+ *       500:
+ *         description: Internal server error
+ */
+deviceRouter.route('/add').post( // Citizen users should have verified token to add devices to their profile via webapp
+  getTokenFromCookie,
+  verifyTokenWithRole('citizen'),
+  addDevice
+);
+
+
+
+deviceRouter.route('/link').post( // Sensor devices that will request for linking with a citizen acct requires bearer token
+  getTokenFromBearer,
+  verifyTokenWithRole('sensor'),
+  linkDevice
+);
+
+
+/**
+ * @swagger
+ * /device/list:
+ *   get:
+ *     summary: Endpoint for getting list of devices associated with the authenticated user (citizen)
+ *     tags:
+ *       - Devices
+ *     security:
+ *       - cookieAuth: []
+ *     responses:
+ *       200:
+ *         description: Successful response with a list of devices as objects in array
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: number
+ *                   description: Status code indicating success
+ *                 message:
+ *                   type: string
+ *                   description: A descriptive message indicating success
+ *                 payload:
+ *                   type: array
+ *                   description: Array of devices associated with the authenticated user
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       network:
+ *                         type: string
+ *                         description: Network code of the device
+ *                       station:
+ *                         type: string
+ *                         description: Station code of the device  
+ *                       status:
+ *                         type: string
+ *                         description: Status of the device (Streaming, Not Streaming, Not Yet Linked)
+ *                       statusSince:
+ *                         type: string
+ *                         description: Timestamp indicating when the device status changed (Not Available if Not Yet Linked)
+ *       500:
+ *         description: Internal server error
+ */
+deviceRouter.route('/list').get( 
+  getTokenFromCookie,
+  verifyTokenWithRole('citizen'),
+  getDeviceList
+);
+
+
 /**
  * @swagger
  * /device/status:
