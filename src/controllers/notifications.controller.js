8c6aab43dd04d2d9a0ada934da9dc8747e46bb83
prokorpio@adride <sanchez.christopherjeff@gmail.com>
2023-08-23 14:25:59 +0800

    chore: set validation error messages for /notifications

--- src/controllers/notifications.controller.js
+++ src/controllers/notifications.controller.js
@@ -1,28 +1,53 @@
 const Joi = require('joi')
 const NotificationsService = require('../services/notifications.service')
 const {responseCodes} = require('./responseCodes')
+const {formatErrorMessage} = require('./helpers')
 
 exports.subscribe = async (req, res, next) => {
   // Define validation Schema
   const subscriptionSchema = Joi.object({
-    endpoint: Joi.string().uri().required(),
-    expirationTime: Joi.date().allow(null).optional(),
+    endpoint: Joi.string()
+      .uri()
+      .required()
+      .messages({
+        "any.required": "Endpoint is required.",
+        "string.uri": "Endpoint must be a valid URI.",
+      }),
+    expirationTime: Joi.date()
+      .allow(null)
+      .optional()
+      .messages({
+        "date.base": "Expiration time must be a valid date.",
+      }),
     keys: Joi.object({
-      p256dh: Joi.string().required(),
-      auth: Joi.string().required()
-    }).required()
+      p256dh: Joi.string()
+        .required()
+        .messages({
+          "any.required": "p256dh key is required.",
+        }),
+      auth: Joi.string()
+        .required()
+        .messages({
+          "any.required": "Auth key is required.",
+        }),
+    }).required(),
   });
 
   try {
 
     // Validate POST input
-    const {error, value} = subscriptionSchema.validate(req.body)
+    const {error, value} = subscriptionSchema.validate(req.body, {abortEarly: false})
     if(error){
-      console.log(error.details[0].message)
+      const errorMessages = error.details.map(
+        (detail) => formatErrorMessage(detail.message)
+      );
+      console.error("Validation Errors:", errorMessages);
+
       res.status(400).json({
         status: responseCodes.GENERIC_ERROR,
-        message: error.details[0].message
+        message: errorMessages[0]
       });
+
       return;
     }
 
