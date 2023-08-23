c46437a70a128eea6fe1839df3b641f600095a38
prokorpio@adride <sanchez.christopherjeff@gmail.com>
2023-08-23 10:57:23 +0800

    chore: set validation error messages for /device/

--- src/controllers/device.controller.js
+++ src/controllers/device.controller.js
@@ -1,6 +1,7 @@
 const Joi = require('joi');
 const DeviceService = require('../services/device.service')
 const {responseCodes} = require('./responseCodes')
+const {formatErrorMessage} = require('./helpers')
 
 exports.getAllDeviceLocations = async (req, res, next) => {
   console.log("Locations of all devices requested");
@@ -41,6 +42,8 @@ exports.getOwnedDevices = async (req, res, next) => {
   console.log('Owned devices requested');
   try {
 
+    // No validation for GET request
+   
     // Perform Task
     const returnObj = await DeviceService.getAccountDevices(req.username);
 
@@ -75,18 +78,33 @@ exports.getDeviceStatus = async (req, res, next) => {
 
   // Define validation schema
   const deviceStatusQuerySchema = Joi.object({
-    network: Joi.string().regex(/^[A-Z]{2}$/).required(),
-    station: Joi.string().regex(/^[A-Z0-9]{3,5}$/).required(),
+    network: Joi.string()
+      .regex(/^[A-Z]{2}$/)
+      .required()
+      .messages({
+        "any.required": "Network is required.",
+        "string.pattern.base": "Invalid network format. Please provide a valid 2-letter uppercase code.",
+      }),
+    station: Joi.string()
+      .regex(/^[A-Z0-9]{3,5}$/)
+      .required()
+      .messages({
+        "any.required": "Station is required.",
+        "string.pattern.base": "Invalid station format. Please provide a valid 3 to 5-character uppercase alphanumeric code.",
+      }),
   });
 
   try {
     // Validate query params
     const {error, value} = deviceStatusQuerySchema.validate(req.query)
     if(error){
-      console.log(error.details[0].message)
+      const errorMessages = error.details.map(
+        (detail) => formatErrorMessage(detail.message)
+      );
+      console.error("Validation Errors:", errorMessages);
       res.status(400).json({
         status: responseCodes.GENERIC_ERROR,
-        message: error.details[0].message
+        message: errorMessages[0]
       });
       return;
     }
@@ -126,11 +144,39 @@ exports.addDevice = async (req, res, next) => {
 
   // Define validation schema
   const addDeviceSchema = Joi.object().keys({
-    network: Joi.string().regex(/^[A-Z]{2}$/).required(),
-    station: Joi.string().regex(/^[A-Z0-9]{3,5}$/).required(),
-    elevation: Joi.string().regex(/^[-+]?\d+(\.\d+)?$/).required(),
-    latitude: Joi.string().regex(/^[-+]?(?:90(?:\.0{1,6})?|(?:[0-8]?\d(?:\.\d{1,6})?))$/).required(),
-    longitude: Joi.string().regex(/^[-+]?(?:180(?:\.0{1,6})?|(?:1[0-7]\d|0?\d{1,2})(?:\.\d{1,6})?)$/).required()
+    network: Joi.string()
+      .regex(/^[a-zA-Z]{2}$/)
+      .required()
+      .messages({
+        "string.pattern.base": "Please provide a valid 2-letter network code.",
+      }),
+    station: Joi.string()
+      .regex(/^[a-zA-Z0-9]{3,5}$/)
+      .required()
+      .messages({
+        "string.pattern.base": "Please provide a valid 3 to 5-character alphanumeric station code.",
+      }),
+    elevation: Joi.string()
+      .regex(/^[-+]?\d+(\.\d+)?$/)
+      .required()
+      .messages({
+        "string.pattern.base": "Please provide a valid elevation value.",
+      }),
+    latitude: Joi.string()
+      .regex(/^[-+]?(?:90(?:\.0{1,6})?|(?:[0-8]?\d(?:\.\d{1,6})?))$/)
+      .required()
+      .messages({
+        "string.pattern.base": "Please provide a valid latitude value.",
+      }),
+    longitude: Joi.string()
+      .regex(/^[-+]?(?:180(?:\.0{1,6})?|(?:1[0-7]\d|0?\d{1,2})(?:\.\d{1,6})?)$/)
+      .required()
+      .messages({
+        "string.pattern.base": "Please provide a valid longitude value.",
+      }),
+  }).messages({ // Default message if no custom message is set for the key
+    "any.required": "{#label} is required.",
+    "string.empty": "{#label} cannot be empty.",
   });
 
   try {
@@ -142,11 +188,16 @@ exports.addDevice = async (req, res, next) => {
     // Validate POST input
     const {error, value} = addDeviceSchema.validate(req.body)
     if(error){
-      console.log(error.details[0].message)
+      const errorMessages = error.details.map(
+        (detail) => formatErrorMessage(detail.message)
+      );
+      console.error("Validation Errors:", errorMessages);
+
       res.status(400).json({
         status: responseCodes.GENERIC_ERROR,
         message: error.details[0].message
       });
+
       return;
     }
     const {network, station, elevation, latitude, longitude} = value
@@ -183,8 +234,19 @@ exports.linkDevice = async (req, res, next) => {
 
   // Define validation schema
   const linkDeviceSchema = Joi.object().keys({
-    macAddress: Joi.string().regex(/^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/).required(),
-    streamId: Joi.string().regex(/^[A-Z]{2}_[A-Z0-9]{5}_.*\/MSEED$/).required()
+    macAddress: Joi.string().regex(/^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/)
+     .required()
+     .messages({
+        "string.pattern.base": "Please provide a valid MAC address string.",
+      }),
+    streamId: Joi.string().regex(/^[A-Z]{2}_[A-Z0-9]{5}_.*\/MSEED$/)
+     .required()
+     .messages({
+        "string.pattern.base": "Please provide a valid Stream ID string.",
+      }),
+  }).messages({ // Default message if no custom message is set for the key
+    "any.required": "{#label} is required.",
+    "string.empty": "{#label} cannot be empty.",
   });
 
   try {
